73ed71c9962499ef8d973cb0702847b81bfa5a92
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2023-06-19 23:19:37 +0700

    commit ges

---
+++ script.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("Ini video ");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("ini audio ");
+})
+
+gambar.addEventListener("click", function gambar(){
+    alert("ini gambar ");
+});
